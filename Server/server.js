484642389684cf9799e137d2f45b7b5f6a6dd776
DhananjayMallik <dhananjaymallik918@gmail.com>
2026-03-17 21:19:06 +0530

    feat : deployment guide for backend folder in my vercel account

--- Server/server.js
+++ Server/server.js
@@ -8,12 +8,17 @@ import taskRouter from './src/routes/taskRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://task-management-system-dh2x-5yxfziard-dhonu-malliks-projects.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
