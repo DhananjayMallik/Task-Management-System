00a130ee325359c251605d60efa8593087e6cc44
DhananjayMallik <dhananjaymallik918@gmail.com>
2026-03-17 21:32:19 +0530

    feat : deployment guide for backend folder in my vercel account

--- Server/server.js
+++ Server/server.js
@@ -11,9 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://task-management-system-fam5.vercel.app"
-    ],
+      "http://localhost:5173",],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
